be2d33555ad1210376d7db5e59aeb9c70b5cbfed
Manaen Vásquez <jio.sander.vc@gmail.comm>
2025-09-04 22:38:31 -0600

    Corrijo rutas en todas las páginas

--- js/ventanas.js
+++ js/ventanas.js
@@ -1,17 +1,17 @@
 function mostrarVentanas(seccion) {
   const datos = {
     auditoria: [
-      { titulo: "Auditoría", url: "../paginas/auditoria.html", img: "../imagenes/auditoria.png", desc: "Introducción a la auditoría informática" },
-      { titulo: "Metodología", url: "../paginas/Metodologia.html", img: "../imagenes/imagenes de paginas/proceso.png", desc: "Pasos y enfoques de auditoría" },
-      { titulo: "Software", url: "../paginas/software.html", img: "../imagenes/software.png", desc: "Herramientas utilizadas" },
-      { titulo: "Normativas", url: "../paginas/Normativas-Estándares.html", img: "../imagenes/normativas.png", desc: "Reglas y estándares aplicables" }
+      { titulo: "Auditoría", url: "paginas/auditoria.html", img: "imagenes/auditoria.png", desc: "Introducción a la auditoría informática" },
+      { titulo: "Metodología", url: "paginas/Metodologia.html", img: "imagenes/imagenes de paginas/proceso.png", desc: "Pasos y enfoques de auditoría" },
+      { titulo: "Software", url: "paginas/software.html", img: "imagenes/software.png", desc: "Herramientas utilizadas" },
+      { titulo: "Normativas", url: "paginas/Normativas-Estándares.html", img: "imagenes/normativas.png", desc: "Reglas y estándares aplicables" }
     ],
     drift: [
-      { titulo: "Historia del Drift", url: "../paginas/drift.html", img: "../imagenes/drift.png", desc: "Orígenes y evolución del drift" },
-      { titulo: "Técnicas", url: "../paginas/Tecnicas.html", img: "../imagenes/tecnicas/shift.png", desc: "Cómo se ejecuta el drift" },
-      { titulo: "Autos", url: "../paginas/Autos.html", img: "../imagenes/drift-setup.png", desc: "Modelos ideales para drifting" },
-      { titulo: "Leyendas", url: "../paginas/leyendas.html", img: "../imagenes/pilotos/tsuchiya.png", desc: "Pilotos icónicos y mitos" },
-      { titulo: "Acerca de", url: "../paginas/acerca.html", img: "../imagenes/acerca.png", desc: "Información del sitio" }
+      { titulo: "Historia del Drift", url: "paginas/drift.html", img: "imagenes/drift.png", desc: "Orígenes y evolución del drift" },
+      { titulo: "Técnicas", url: "paginas/Tecnicas.html", img: "imagenes/tecnicas/shift.png", desc: "Cómo se ejecuta el drift" },
+      { titulo: "Autos", url: "paginas/Autos.html", img: "imagenes/drift-setup.png", desc: "Modelos ideales para drifting" },
+      { titulo: "Leyendas", url: "paginas/leyendas.html", img: "imagenes/pilotos/tsuchiya.png", desc: "Pilotos icónicos y mitos" },
+      { titulo: "Acerca de", url: "paginas/acerca.html", img: "imagenes/acerca.png", desc: "Información del sitio" }
     ]
   };
 
